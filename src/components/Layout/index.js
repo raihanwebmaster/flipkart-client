65bb6ae4bd6432d8cc5f00258e5007fab0c89399
raihanwebmaster <raihanemon2015@gmail.com>
2021-06-15 23:10:26 +0200

    fetch product api and render product list

---
+++ src/components/Layout/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Header';
+import MenuHeader from '../MenuHeader';
+const Layout = (props) => {
+    return (
+        <>
+           <Header />
+           <MenuHeader /> 
+           {props.children}
+        </>
+    )
+}
+
+export default Layout;
